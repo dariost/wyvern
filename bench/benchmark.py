b2f2aaa3db56588313860547cc1a8d63b669fde8
Dario Ostuni <dario.ostuni@gmail.com>
2018-10-01 22:24:30 +0200

    Export data from benchmark

--- bench/benchmark.py
+++ bench/benchmark.py
@@ -10,15 +10,20 @@ if __name__ == "__main__":
     path = sys.argv[1]
     data = open(path + "/benchmark.json", "r").read()
     data = json.loads(data)
+    output = {"name": data["name"]}
     print("Benchmarking", data["name"])
     max_lenght = max(map(len, data["executors"]))
     for test in data["tests"]:
         print("Test(", end='')
+        pp = ""
         for i, param in enumerate(data["parameters"]):
             print(param, "=", test[i], sep='', end='')
+            pp += str(test[i])
             if(i != len(data["parameters"]) - 1):
                 print(", ", end='')
+                pp += "|"
         print("):")
+        output[pp] = {}
         for executor, commands in data["executors"].items():
             formatted = (" " * (max_lenght - len(executor))) + executor
             cumulative_time = 0.0
@@ -43,3 +48,5 @@ if __name__ == "__main__":
                 results.append(result_time)
             avg_time = sum(results) / len(results)
             print(formatted, "=>", "%.6f" % (avg_time,))
+            output[pp][executor] = avg_time
+    print(json.dumps(output), file=sys.stderr)
