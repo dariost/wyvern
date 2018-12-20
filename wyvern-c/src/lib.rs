ceb28808cb46b872c697f1cc0c758d90b30a7b5e
Dario Ostuni <dario.ostuni@gmail.com>
2018-12-20 18:54:06 +0100

    Use cbindgen to generate Wyvern C bindings

--- wyvern-c/src/lib.rs
+++ wyvern-c/src/lib.rs
@@ -1,10 +1,12 @@
+#![allow(non_camel_case_types)]
+
 extern crate libc;
 extern crate serde_json;
 extern crate wyvern;
 
 use libc::{free, malloc};
-use std::ffi::c_void;
 use std::ffi::CStr;
+use std::ffi::c_void;
 use std::os::raw::c_char;
 use std::ptr::write;
 use std::slice;
@@ -19,67 +21,89 @@ use wyvern::vk::resource::VkResource;
 pub const WYVERN_INPUT: u32 = 0;
 pub const WYVERN_OUTPUT: u32 = 1;
 
+#[repr(C)]
+pub struct wyvern_data_array_uint32_t {
+    size: u32,
+    data: *const u32,
+}
+
+#[repr(C)]
+pub struct wyvern_data_array_int32_t {
+    size: u32,
+    data: *const i32,
+}
+
+#[repr(C)]
+pub struct wyvern_data_array_float_t {
+    size: u32,
+    data: *const f32,
+}
+
+pub type wyvern_vk_executor_t = c_void;
+pub type wyvern_vk_executable_t = c_void;
+pub type wyvern_vk_resource_t = c_void;
+
 #[no_mangle]
-pub unsafe fn wyvern_vk_executor_new() -> *mut VkExecutor {
+pub unsafe extern "C" fn wyvern_vk_executor_new() -> *mut wyvern_vk_executor_t {
     let executor = Box::new(VkExecutor::new(Default::default()).unwrap());
-    Box::into_raw(executor)
+    Box::into_raw(executor) as *mut c_void
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_executor_destroy(obj: *mut VkExecutor) {
-    Box::from_raw(obj);
+pub unsafe extern "C" fn wyvern_vk_executor_destroy(obj: *mut wyvern_vk_executor_t) {
+    Box::from_raw(obj as *mut VkExecutor);
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_executable_new(
-    obj: *mut VkExecutor,
+pub unsafe extern "C" fn wyvern_vk_executable_new(
+    obj: *mut wyvern_vk_executor_t,
     source: *const c_char,
-) -> *mut VkExecutable {
-    let obj = &mut *obj;
+) -> *mut wyvern_vk_executable_t {
+    let obj = &mut *(obj as *mut VkExecutor);
     let source = CStr::from_ptr(source).to_str().unwrap();
     let program: Program = serde_json::from_str(source).unwrap();
     let executable = Box::new(obj.compile(program).unwrap());
-    Box::into_raw(executable)
+    Box::into_raw(executable) as *mut c_void
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_executable_destroy(obj: *mut VkExecutor) {
-    Box::from_raw(obj);
+pub unsafe extern "C" fn wyvern_vk_executable_destroy(obj: *mut wyvern_vk_executor_t) {
+    Box::from_raw(obj as *mut VkExecutor);
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_resource_new(obj: *mut VkExecutor) -> *mut Arc<VkResource> {
-    let obj = &mut *obj;
+pub unsafe extern "C" fn wyvern_vk_resource_new(obj: *mut wyvern_vk_executor_t) -> *mut wyvern_vk_resource_t {
+    let obj = &mut *(obj as *mut VkExecutor);
     let resource = Box::new(obj.new_resource().unwrap());
-    Box::into_raw(resource)
+    Box::into_raw(resource) as *mut c_void
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_resource_destroy(obj: *mut Arc<VkResource>) {
-    Box::from_raw(obj);
+pub unsafe extern "C" fn wyvern_vk_resource_destroy(obj: *mut wyvern_vk_resource_t) {
+    Box::from_raw(obj as *mut Arc<VkResource>);
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_executable_bind(
-    obj: *mut VkExecutable,
+pub unsafe extern "C" fn wyvern_vk_executable_bind(
+    obj: *mut wyvern_vk_executable_t,
     name: *const c_char,
     kind: u32,
-    resource: *mut Arc<VkResource>,
+    resource: *mut wyvern_vk_resource_t,
 ) {
-    let obj = &mut *obj;
+    let obj = &mut *(obj as *mut VkExecutable);
     let name = CStr::from_ptr(name).to_str().unwrap();
     let kind = match kind {
         WYVERN_INPUT => IO::Input,
         WYVERN_OUTPUT => IO::Output,
         _ => panic!(),
     };
-    let resource = &mut *resource;
+    let resource = &mut *(resource as *mut Arc<VkResource>);
     obj.bind(name, kind, resource.clone());
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_executable_unbind(obj: *mut VkExecutable, name: *const c_char, kind: u32) {
-    let obj = &mut *obj;
+pub unsafe extern "C" fn wyvern_vk_executable_unbind(obj: *mut wyvern_vk_executable_t, name: *const c_char, kind: u32) {
+    let obj = &mut *(obj as *mut VkExecutable);
     let name = CStr::from_ptr(name).to_str().unwrap();
     let kind = match kind {
         WYVERN_INPUT => IO::Input,
@@ -90,65 +114,65 @@ pub unsafe fn wyvern_vk_executable_unbind(obj: *mut VkExecutable, name: *const c
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_executable_run(obj: *mut VkExecutable) {
-    let obj = &mut *obj;
+pub unsafe extern "C" fn wyvern_vk_executable_run(obj: *mut wyvern_vk_executable_t) {
+    let obj = &mut *(obj as *mut VkExecutable);
     obj.run().unwrap();
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_resource_set_data_uint32(obj: *mut Arc<VkResource>, data: u32) {
-    let obj = &mut *obj;
+pub unsafe extern "C" fn wyvern_vk_resource_set_data_uint32(obj: *mut wyvern_vk_resource_t, data: u32) {
+    let obj = &mut *(obj as *mut Arc<VkResource>);
     obj.set_data(TokenValue::Scalar(ConstantScalar::U32(data)));
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_resource_set_data_int32(obj: *mut Arc<VkResource>, data: i32) {
-    let obj = &mut *obj;
+pub unsafe extern "C" fn wyvern_vk_resource_set_data_int32(obj: *mut wyvern_vk_resource_t, data: i32) {
+    let obj = &mut *(obj as *mut Arc<VkResource>);
     obj.set_data(TokenValue::Scalar(ConstantScalar::I32(data)));
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_resource_set_data_float32(obj: *mut Arc<VkResource>, data: f32) {
-    let obj = &mut *obj;
+pub unsafe extern "C" fn wyvern_vk_resource_set_data_float32(obj: *mut wyvern_vk_resource_t, data: f32) {
+    let obj = &mut *(obj as *mut Arc<VkResource>);
     obj.set_data(TokenValue::Scalar(ConstantScalar::F32(data)));
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_resource_set_data_array_uint32(
-    obj: *mut Arc<VkResource>,
+pub unsafe extern "C" fn wyvern_vk_resource_set_data_array_uint32(
+    obj: *mut wyvern_vk_resource_t,
     data: *const u32,
-    n_elem: usize,
+    n_elem: u32,
 ) {
-    let obj = &mut *obj;
-    let data = slice::from_raw_parts(data, n_elem);
+    let obj = &mut *(obj as *mut Arc<VkResource>);
+    let data = slice::from_raw_parts(data, n_elem as usize);
     obj.set_data(TokenValue::Vector(ConstantVector::U32(data.to_vec())));
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_resource_set_data_array_int32(
-    obj: *mut Arc<VkResource>,
+pub unsafe extern "C" fn wyvern_vk_resource_set_data_array_int32(
+    obj: *mut wyvern_vk_resource_t,
     data: *const i32,
-    n_elem: usize,
+    n_elem: u32,
 ) {
-    let obj = &mut *obj;
-    let data = slice::from_raw_parts(data, n_elem);
+    let obj = &mut *(obj as *mut Arc<VkResource>);
+    let data = slice::from_raw_parts(data, n_elem as usize);
     obj.set_data(TokenValue::Vector(ConstantVector::I32(data.to_vec())));
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_resource_set_data_array_float32(
-    obj: *mut Arc<VkResource>,
+pub unsafe extern "C" fn wyvern_vk_resource_set_data_array_float32(
+    obj: *mut wyvern_vk_resource_t,
     data: *const f32,
-    n_elem: usize,
+    n_elem: u32,
 ) {
-    let obj = &mut *obj;
-    let data = slice::from_raw_parts(data, n_elem);
+    let obj = &mut *(obj as *mut Arc<VkResource>);
+    let data = slice::from_raw_parts(data, n_elem as usize);
     obj.set_data(TokenValue::Vector(ConstantVector::F32(data.to_vec())));
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_resource_get_data_uint32(obj: *mut Arc<VkResource>) -> u32 {
-    let obj = &mut *obj;
+pub unsafe extern "C" fn wyvern_vk_resource_get_data_uint32(obj: *mut wyvern_vk_resource_t) -> u32 {
+    let obj = &mut *(obj as *mut Arc<VkResource>);
     if let TokenValue::Scalar(ConstantScalar::U32(value)) = obj.get_data() {
         value
     } else {
@@ -157,8 +181,8 @@ pub unsafe fn wyvern_vk_resource_get_data_uint32(obj: *mut Arc<VkResource>) -> u
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_resource_get_data_int32(obj: *mut Arc<VkResource>) -> i32 {
-    let obj = &mut *obj;
+pub unsafe extern "C" fn wyvern_vk_resource_get_data_int32(obj: *mut wyvern_vk_resource_t) -> i32 {
+    let obj = &mut *(obj as *mut Arc<VkResource>);
     if let TokenValue::Scalar(ConstantScalar::I32(value)) = obj.get_data() {
         value
     } else {
@@ -167,8 +191,8 @@ pub unsafe fn wyvern_vk_resource_get_data_int32(obj: *mut Arc<VkResource>) -> i3
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_resource_get_data_float32(obj: *mut Arc<VkResource>) -> f32 {
-    let obj = &mut *obj;
+pub unsafe extern "C" fn wyvern_vk_resource_get_data_float32(obj: *mut wyvern_vk_resource_t) -> f32 {
+    let obj = &mut *(obj as *mut Arc<VkResource>);
     if let TokenValue::Scalar(ConstantScalar::F32(value)) = obj.get_data() {
         value
     } else {
@@ -177,61 +201,67 @@ pub unsafe fn wyvern_vk_resource_get_data_float32(obj: *mut Arc<VkResource>) -> 
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_resource_get_data_array_uint32(obj: *mut Arc<VkResource>) -> *mut c_void {
-    let obj = &mut *obj;
+pub unsafe extern "C" fn wyvern_vk_resource_get_data_array_uint32(obj: *mut wyvern_vk_resource_t) -> wyvern_data_array_uint32_t {
+    let obj = &mut *(obj as *mut Arc<VkResource>);
     if let TokenValue::Vector(ConstantVector::U32(value)) = obj.get_data() {
-        let data = malloc((value.len() + 1) * 4) as *mut u32;
-        write(data, value.len() as u32);
+        let data = malloc(value.len() * 4) as *mut u32;
         for i in 0..value.len() {
-            write(data.offset(i as isize + 1), value[i]);
+            write(data.offset(i as isize), value[i]);
         }
-        data as *mut c_void
+        wyvern_data_array_uint32_t {
+            size: value.len() as u32,
+            data: data,
+        }
     } else {
         panic!("Wrong type requested!");
     }
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_resource_get_data_array_int32(obj: *mut Arc<VkResource>) -> *mut c_void {
-    let obj = &mut *obj;
+pub unsafe extern "C" fn wyvern_vk_resource_get_data_array_int32(obj: *mut wyvern_vk_resource_t) -> wyvern_data_array_int32_t {
+    let obj = &mut *(obj as *mut Arc<VkResource>);
     if let TokenValue::Vector(ConstantVector::I32(value)) = obj.get_data() {
-        let data = malloc((value.len() + 1) * 4) as *mut i32;
-        write(data, value.len() as i32);
+        let data = malloc(value.len() * 4) as *mut i32;
         for i in 0..value.len() {
-            write(data.offset(i as isize + 1), value[i]);
+            write(data.offset(i as isize), value[i]);
         }
-        data as *mut c_void
+        wyvern_data_array_int32_t {
+            size: value.len() as u32,
+            data: data,
+        }
     } else {
         panic!("Wrong type requested!");
     }
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_resource_get_data_array_float32(obj: *mut Arc<VkResource>) -> *mut c_void {
-    let obj = &mut *obj;
+pub unsafe extern "C" fn wyvern_vk_resource_get_data_array_float32(obj: *mut wyvern_vk_resource_t) -> wyvern_data_array_float_t {
+    let obj = &mut *(obj as *mut Arc<VkResource>);
     if let TokenValue::Vector(ConstantVector::F32(value)) = obj.get_data() {
-        let data = malloc((value.len() + 1) * 4) as *mut f32;
-        write(data, f32::from_bits(value.len() as u32));
+        let data = malloc(value.len() * 4) as *mut f32;
         for i in 0..value.len() {
-            write(data.offset(i as isize + 1), value[i]);
+            write(data.offset(i as isize), value[i]);
         }
-        data as *mut c_void
+        wyvern_data_array_float_t {
+            size: value.len() as u32,
+            data: data,
+        }
     } else {
         panic!("Wrong type requested!");
     }
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_resource_data_array_uint32_free(data: *mut c_void) {
-    free(data as *mut libc::c_void);
+pub unsafe extern "C" fn wyvern_vk_resource_data_array_uint32_free(data: wyvern_data_array_uint32_t) {
+    free(data.data as *mut libc::c_void);
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_resource_data_array_int32_free(data: *mut c_void) {
-    free(data as *mut libc::c_void);
+pub unsafe extern "C" fn wyvern_vk_resource_data_array_int32_free(data: wyvern_data_array_int32_t) {
+    free(data.data as *mut libc::c_void);
 }
 
 #[no_mangle]
-pub unsafe fn wyvern_vk_resource_data_array_float32_free(data: *mut c_void) {
-    free(data as *mut libc::c_void);
+pub unsafe extern "C" fn wyvern_vk_resource_data_array_float32_free(data: wyvern_data_array_float_t) {
+    free(data.data as *mut libc::c_void);
 }
